2f2847f72dd5a63cdda8f9ca4311ab7e8eef4da9
Tom-on64 <kubecekt@gmail.com>
2024-06-08 17:02:52 +0200

    Added dump and load command to monitor

--- emu/emu.c
+++ emu/emu.c
@@ -28,17 +28,6 @@ int main(int argc, char** argv) {
     
     printf("\nEMU %s monitor - Type 'help' for more information\n\n", VERSION);
 
-    /*
-    for (int i = 0; i < 256; i++) {
-        if (i % 16 == 0) {
-            printf("\n0x%04x: ", i);
-        }
-
-        printf("%02x ", state.memory.raw[i]);
-    }
-    printf("\n");
-    */
-
     while (1) {
         char buf[MAX_INPUT] = { 0 };
         char out[MAX_OUTPUT] = { 0 };
@@ -82,12 +71,32 @@ void command(state_t* state, char* command, char* out) {
     if (tokenCount == 0) {
         return;
     } else if (strcmp(tokens[0], "cls") == 0) {
-        strcpy(out, "\x1b[H\x1b[J");
+        strcpy(out, "\x1b[H\x1b[J"); // Clear screen
+    } else if (strcmp(tokens[0], "dump") == 0) {
+        if (tokenCount < 2) {
+            strcpy(out, "Expected address!\n");
+            return;
+        }
+
+        uint16_t addr = (uint16_t)strtoul(tokens[1], NULL, 0);
+        uint16_t len = 256;
+
+        if (tokenCount >= 3) len = (uint16_t)strtoul(tokens[2], NULL, 0);
+
+        for (int i = 0; i < len; i++) {
+            if (i % 16 == 0) {
+                printf("\n0x%04x: ", addr + i);
+            }
+
+            printf("%02x ", state->memory.raw[addr + i]);
+        }
+        printf("\n");
+
     } else if (strcmp(tokens[0], "exit") == 0) {
         exit(0);
     } else if (strcmp(tokens[0], "goto") == 0) {
         if (tokenCount < 2) {
-            strcpy(out, "Expected argument!\n");
+            strcpy(out, "Expected address!\n");
             return;
         }
 
@@ -102,10 +111,12 @@ void command(state_t* state, char* command, char* out) {
     } else if (strcmp(tokens[0], "help") == 0) {
         strcpy(out, "EMU " VERSION " monitor help:\n"
                 " cls               Clear monitor output\n"
+                " dump <addr> [num] Prints num bytes at address (256 by default)\n"
                 " exit              Stops the emulator and exits\n"
                 " goto <addr>       Sets PC to address\n"
                 " halt              Toggles the halt bit\n"
                 " help              Prints this help message\n"
+                " load <file> [loc] Loads a file into loc (0x0000 by default)\n"
                 " peek <addr>       Prints byte at memory address\n"
                 " poke <addr> <val> Sets a byte at memory address\n"
                 //" reboot            Reboots the system\n"
@@ -113,9 +124,24 @@ void command(state_t* state, char* command, char* out) {
                 " run               Runs until halt\n"
                 " step              Runs a single clock cycle\n"
               );
+    } else if (strcmp(tokens[0], "load") == 0) {
+        if (tokenCount < 2) {
+            strcpy(out, "Expected filename!\n");
+            return;
+        }
+
+        uint16_t addr = 0;
+        if (tokenCount >= 3) addr = strtoul(tokens[2], NULL, 0);
+        
+        if (load(state, tokens[1], addr) != 0) {
+            sprintf(out, "%s not found!\n", tokens[1]);
+            return;
+        }
+
+        sprintf(out, "%s loaded at 0x%04x\n", tokens[1], addr);
     } else if (strcmp(tokens[0], "peek") == 0) {
         if (tokenCount < 2) {
-            strcpy(out, "Expected argument!\n");
+            strcpy(out, "Expected address!\n");
             return;
         }
 
@@ -124,7 +150,7 @@ void command(state_t* state, char* command, char* out) {
         sprintf(out, "0x%04x: 0x%02x %4s\n", addr, value, I_NAME(value));
     } else if (strcmp(tokens[0], "poke") == 0) {
         if (tokenCount < 3) {
-            strcpy(out, "Expected argument!\n");
+            strcpy(out, "Expected address/value!\n");
             return;
         }
 
