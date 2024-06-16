f7eac71bad9a22359dd7d5b0a2bc9ee66ee01e6f
Tom-on64 <kubecekt@gmail.com>
2024-06-16 15:50:19 +0200

    Added a basic assembler thing (doesn't do anything)

--- as/as.c
+++ as/as.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "as.h"
 
 int main(int argc, char** argv) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
         return 1;
     }
+
+    char* buf;
+    FILE* fp; 
+    if ((fp = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "File not found!\n");
+        return 1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    size_t len = ftell(fp);
+    rewind(fp);
+    if ((buf = malloc(len)) == NULL) {
+        fprintf(stderr, "Not enough memory!\n");
+        return 1;
+    }
+
+    if (fread(buf, len, 1, fp) != 1) {
+        fprintf(stderr, "Failed while reading file!\n");
+        return 1;
+    }
+
+    assemble(buf, len);
     
-    printf("Assembling %s...\n", argv[1]);
+    fclose(fp);
+    free(buf);
 
     return 0;
 }
+
+int assemble(char* code, size_t len) {
+    printf("Filesize: %lu Bytes\n", len);
+
+    return 0;
+}
+
---
+++ as/as.h
@@ -0,0 +1,6 @@
+#ifndef AS_H
+#define AS_H
+
+int assemble(char*, size_t);
+
+#endif
